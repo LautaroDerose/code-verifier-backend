47b24f2859745c3b252e324c086a1cd5513c52bb
Lautaro <lautiderose@gmail.com>
2023-07-21 19:53:23 -0300

    Server config done, Cors & helmet basic config & Root Router Ready

--- dist/index.js
+++ dist/index.js
@@ -3,40 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const server_1 = __importDefault(require("./src/server"));
+const logger_1 = require("./src/utils/logger");
 //Configuration the .env file
 dotenv_1.default.config();
-//  Create Express APP
-const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
-// Define the first Route of APP
-app.get('/', (req, res) => {
-    // Send Hello World
-    res.send('Welcome to Api Restful Express + Nodemon + Jest + TS + Swagger + Mongoose ');
+// * Execute SERVER
+server_1.default.listen(port, () => {
+    (0, logger_1.LogSuccess)(`[SERVER ON]: Running in http://localhost:${port}/api`);
 });
-// Define the first Route of APP
-app.get('/hello', (req, res) => {
-    // Send Hello World
-    res.send('Welcome to GET Route: Hello World! ');
+// * Conntrol SERVER ERROR
+server_1.default.on('error', (error) => {
+    (0, logger_1.LogError)(`[SERVER ERROR]: ${error}`);
 });
-//Execute App and Listen Requests to PORT
-app.listen(port, () => console.log(`EXPRESS SERVER: Running at http://localhost:${port}`));
-/////////////////////////////////////////////////////////////////////
-//  ejercicio1:
-// app.get('/ejercicio1', (req: Request, res: Response) => {
-//   const data = {
-//     message: 'Goobye, world'
-//   };
-//   res.status(200).json({ data });
-// });
-// ejercicio 2
-// app.get('/ejercicio2', (req: Request, res: Response) => {
-//   const name = req.query.name || 'Anonimo';
-//   const data = {
-//     message: `Hola, ${name}`
-//   };
-//   res.status(200).json({ data });
-// });
-////////////////////////////////////////////////////////////////////
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,46 +1,19 @@
-import express, { Express, Request, Response } from "express";
 import dotenv from 'dotenv';
+import server from './src/server'
+import { LogError, LogSuccess } from './src/utils/logger';
 
 //Configuration the .env file
 dotenv.config();
 
-//  Create Express APP
-const app: Express = express();
-const port: string | number = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
-// Define the first Route of APP
-app.get('/', (req: Request, res: Response) => {
-  // Send Hello World
-  res.send('Welcome to Api Restful Express + Nodemon + Jest + TS + Swagger + Mongoose ')
+// * Execute SERVER
+server.listen(port, () => {
+  LogSuccess(`[SERVER ON]: Running in http://localhost:${port}/api`)
 });
 
-// Define the first Route of APP
-app.get('/hello', (req: Request, res: Response) => {
-  // Send Hello World
-  res.send('Welcome to GET Route: Hello World! ')
+// * Conntrol SERVER ERROR
+server.on('error', (error) => {
+  LogError(`[SERVER ERROR]: ${error}`);
 });
 
-
-//Execute App and Listen Requests to PORT
-app.listen(port, () => 
-console.log(`EXPRESS SERVER: Running at http://localhost:${port}`)
-)
-
-
-/////////////////////////////////////////////////////////////////////
-//  ejercicio1:
-// app.get('/ejercicio1', (req: Request, res: Response) => {
-//   const data = {
-//     message: 'Goobye, world'
-//   };
-//   res.status(200).json({ data });
-// });
-// ejercicio 2
-// app.get('/ejercicio2', (req: Request, res: Response) => {
-//   const name = req.query.name || 'Anonimo';
-//   const data = {
-//     message: `Hola, ${name}`
-//   };
-//   res.status(200).json({ data });
-// });
-////////////////////////////////////////////////////////////////////
